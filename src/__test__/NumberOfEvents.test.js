af72e46120baa59dc27755065aad5e0cb9df260c
andreawork-hub <andrea.vitova@hotmail.com>
2023-02-21 15:20:55 +0100

    Jest config

--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -7,7 +7,7 @@ describe('<NumberOfEvents/> component', () => {
     let NOEWrapper
     beforeAll(() => {
         NOEWrapper = shallow(
-            <NumberOfEvents />
+            <NumberOfEvents updateEvents={() => { }} />
         )
     })
 
@@ -17,7 +17,7 @@ describe('<NumberOfEvents/> component', () => {
 
     test('user can change NOE', () => {
         const input = NOEWrapper.find('input.number');
-        const inputValue = { target: { value: 20 } };
+        const inputValue = { target: { value: 16 } };
         input.simulate('change', inputValue);
         expect(NOEWrapper.state('NOE')).toBe(16);
     })

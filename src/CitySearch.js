502cd94bad5e98c7564b96d6ca962cbd86f450b2
andreawork-hub <andrea.vitova@hotmail.com>
2023-02-28 18:13:06 +0100

    object-oriented programming

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
         query: 'Berlin',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: undefined,
+        infoText: '',
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({ query: value, suggestions });
-    };
+
+        if (suggestions.lenght === 0) {
+            this.setState({
+                query: value,
+                infoText: 'We cannot find the city you are looking for. Please try another city.',
+            })
+        } else {
+            return this.setState({
+                query: value, suggestions,
+                infoText: ''
+            })
+        };
+    }
 
     handleItemClicked = (suggestion) => {
         this.setState({
             query: suggestion,
-            showSuggestions: false
+            suggestions: [],
+            showSuggestions: false,
+            infoText: ''
         });
         this.props.updateEvents(suggestion);
     }
@@ -33,6 +49,7 @@ class CitySearch extends Component {
                     onChange={this.handleInputChanged}
                     onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
+                <InfoAlert text={this.state.infoText} />
                 <ul className='suggestions' style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
                         <li
